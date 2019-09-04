f2153cbce4174814388f09a4adbfb485b9ce6d78
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-09-04 12:02:55 +0300

    changes on client take place only after successful change on DB

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
+const mongodb = require('mongodb')
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://AndrewBubnov:acnot88_0175A@cluster0-shard-00-00-edszp.mongodb.net:27017,cluster0-shard-00-01-' +
     'edszp.mongodb.net:27017,cluster0-shard-00-02-edszp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 let currentDB;
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,31 +16,30 @@ app.use(compression());
 
 app.get('', async (req, res) => {
     const contacts = await currentDB.collection('contacts').find().toArray()
-    res.send(JSON.stringify(contacts));
+    res.send(contacts);
 })
 
 app.post('/api', (req, res) => {
     currentDB.collection('contacts').insertOne(req.body, (err, result) => {
         if (err) console.log(err);
+        res.send(req.body);
     });
-    res.send(JSON.stringify({status: 'Ok'}));
 });
 
 app.put('/api', (req, res) => {
-    const contact = {id: Number(req.body.id)}
     const set = {}
     const newKeys = Object.keys(req.body).filter(item => item.substring(0, 2) !== '$$' && item.substring(0, 1) !== '_')
     newKeys.forEach(item => set[item] = req.body[item])
-    currentDB.collection('contacts').updateOne(contact, { $set: set }, (err, res) => {
+    currentDB.collection('contacts').updateOne({_id: mongodb.ObjectID(req.body._id)}, { $set: set }, (err, result) => {
         if (err) console.log(err);
+        res.send(200)
     })
 });
 
-app.delete('/api/:id', (req,res) => {
-    const contact = {id: Number(req.params.id)}
-
-    currentDB.collection('contacts').deleteOne(contact, (err, result) => {
+app.delete('/api/:id', (req, res) => {
+    currentDB.collection('contacts').deleteOne({_id: mongodb.ObjectID(req.params.id)}, (err, result) => {
         if (err) console.log(err);
+        res.send(200)
     });
 })
 
