c466c712ab9e1d2403ee43ca164fb42576521d4b
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-09-05 10:52:15 +0300

    one running server for application, server side validation on add item added and controller moved to separate file.

---
+++ client/app/controllers/contactListController.js
@@ -0,0 +1,69 @@
+const app = angular.module("contactList", [])
+app.controller('contactListController', ($scope, $http) => {
+    $scope.items = [];
+    $scope.serverErrorMessage = '';
+    $scope.serverError = false;
+    const PORT = '/contacts/'
+
+    $scope.getError = function (error) {
+        if (angular.isDefined(error)) {
+            if (error.required) {
+                return "Empty field";
+            }
+        }
+    }
+
+    $http.get(PORT)
+        .then(response => {
+            $scope.items = response.data
+        })
+        .catch(err => console.log('Error: ', err));
+
+    $scope.addItem = (newContact, isValid) => {
+        if (isValid) {
+            const contact = JSON.parse(JSON.stringify(newContact))
+            contact.fullName = contact.firstName + ' ' + contact.lastName;
+            $http.post(PORT + 'add', contact)
+                .then(response => {
+                    console.log(response);
+                    $scope.items.push(response.data)
+                    Object.keys(newContact).forEach(item => newContact[item] = '');
+                    $scope.showError = false;
+                })
+                .catch(err => {
+                    $scope.serverErrorMessage = err.data;
+                    $scope.serverError = true;
+                });
+        } else {
+            $scope.showError = true;
+        }
+    }
+
+    $scope.deleteItem = (id) => {
+        $http.delete(PORT + 'delete/' + id)
+            .then(response => {
+                const element = $scope.items.find(element => element.id === id);
+                $scope.items.splice($scope.items.indexOf(element), 1)
+            })
+            .catch(err => console.log('Error: ', err))
+    }
+
+    $scope.editItem = (_id) => {
+        $scope.items.find(element => element._id === _id).edited = true
+    }
+
+    $scope.saveItem = (item) => {
+        const element = $scope.items.find(element => element._id === item._id);
+        item.fullName = item.firstName + ' ' + item.lastName
+        element.edited = false
+        $http.put(PORT + 'edit', item)
+            .then(response => {
+                $scope.items.splice($scope.items.indexOf(element), 1, item)
+            })
+            .catch(err => console.log('Error: ', err))
+    }
+
+    $scope.hideServerErrorMessage = () => {
+        $scope.serverError = false;
+    }
+})
--- server.js
+++ server.js
@@ -3,43 +3,63 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
-const mongodb = require('mongodb')
+const mongodb = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://AndrewBubnov:acnot88_0175A@cluster0-shard-00-00-edszp.mongodb.net:27017,cluster0-shard-00-01-' +
     'edszp.mongodb.net:27017,cluster0-shard-00-02-edszp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 const PORT = process.env.PORT || 3000;
 let currentDB;
+const fields = ['firstName', 'lastName', 'phone', 'cellPhone', 'address', 'fullName'];
+const letters = /[a-zA-Z]+/;
+const phoneNumber = /^\+?[0-9]{10}/
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression());
+app.use(express.static(__dirname + '/client/app/'));
 
-app.get('', async (req, res) => {
-    const contacts = await currentDB.collection('contacts').find().toArray()
-    res.send(contacts);
+app.get('/contacts', async (req, res) => {
+    try {
+        const contacts = await currentDB.collection('contacts').find().toArray();
+        res.send(contacts);
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-app.post('/api', (req, res) => {
-    currentDB.collection('contacts').insertOne(req.body, (err, result) => {
-        if (err) console.log(err);
-        res.send(req.body);
-    });
+app.post('/contacts/add', (req, res) => {
+    if (JSON.stringify(Object.keys(req.body)) === JSON.stringify(fields)) {
+        if (req.body.firstName.match(letters) && req.body.lastName.match(letters)) {
+            if (req.body.phone.match(phoneNumber) && req.body.cellPhone.match(phoneNumber)){
+                currentDB.collection('contacts').insertOne(req.body, (err, result) => {
+                    if (err) res.sendStatus(503);
+                    res.send(req.body);
+                });
+            } else {
+                res.status(503).send('Please enter phone and cell phone in valid format')
+            }
+        } else {
+            res.status(503).send('First name and last name fields should consist of Latin alphabet letters only')
+        }
+
+    }
+
 });
 
-app.put('/api', (req, res) => {
+app.put('/contacts/edit', (req, res) => {
     const set = {}
     const newKeys = Object.keys(req.body).filter(item => item.substring(0, 2) !== '$$' && item.substring(0, 1) !== '_')
     newKeys.forEach(item => set[item] = req.body[item])
     currentDB.collection('contacts').updateOne({_id: mongodb.ObjectID(req.body._id)}, { $set: set }, (err, result) => {
-        if (err) console.log(err);
-        res.send(200)
+        if (err) res.sendStatus(503);
+        res.sendStatus(200)
     })
 });
 
-app.delete('/api/:id', (req, res) => {
+app.delete('/contacts/delete/:id', (req, res) => {
     currentDB.collection('contacts').deleteOne({_id: mongodb.ObjectID(req.params.id)}, (err, result) => {
-        if (err) console.log(err);
-        res.send(200)
+        if (err) res.sendStatus(503);
+        res.sendStatus(200)
     });
 })
 
